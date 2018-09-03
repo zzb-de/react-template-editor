f11ef0074d021a62ba924ba546883a03afb93975
Konstantin Hungs <konstantin.hungs@beuth-hochschule.de>
2018-09-03 14:13:58 +0200

    Add enzyme tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,20 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+
+import { configure, shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+
 import App from "./App";
+
+configure({ adapter: new Adapter() });
 
 it("renders without crashing", () => {
     const div = document.createElement("div");
     ReactDOM.render(<App />, div);
     ReactDOM.unmountComponentAtNode(div);
+});
+
+it("contains the text 'Test'", () => {
+    const app = shallow(<App />);
+    expect(app.text()).toEqual("Test");
 });
