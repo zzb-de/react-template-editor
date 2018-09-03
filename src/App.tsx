faaa80b83a1aadc95d2d209db07a4a89aabdbf98
Konstantin Hungs <konstantin.hungs@beuth-hochschule.de>
2018-09-03 12:35:18 +0200

    Initial create-react-app setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import "./App.css";
+
+class App extends React.Component {
+    render() {
+        return (
+            <div/>
+        );
+    }
+}
+
+export default App;
