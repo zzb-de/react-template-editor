b429ab8f9159744769226ec2e9b2816de0de927a
Sebastian Rittau <srittau@rittau.biz>
2018-09-03 15:37:47 +0200

    Add common enzyme setup file for jest

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,12 +1,9 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
-import { configure, shallow } from "enzyme";
-import Adapter from "enzyme-adapter-react-16";
+import { shallow } from "enzyme";
 
 import App from "./App";
-
-configure({ adapter: new Adapter() });
 
 it("renders without crashing", () => {
     const div = document.createElement("div");
