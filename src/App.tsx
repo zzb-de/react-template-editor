466b85e1e2f4655b7460803de2cc9b88fe57a951
Konstantin Hungs <konstantin.hungs@beuth-hochschule.de>
2018-09-21 14:50:33 +0200

    Change to AppProps

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,11 @@ import { EditorState } from "draft-js";
 import * as React from "react";
 import { RTEEditor } from "./RTEEditor";
 
-interface MyEditorProps {
+interface AppProps {
 }
 
-export class App extends React.Component<MyEditorProps, any> {
-    constructor(props: MyEditorProps) {
+export class App extends React.Component<AppProps, any> {
+    constructor(props: AppProps) {
         super(props);
         this.state = {editorState: EditorState.createEmpty()};
     }
