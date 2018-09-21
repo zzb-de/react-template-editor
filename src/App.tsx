3a7522f00f90f7719d4ab8d8973d0b5644b6e00f
Konstantin Hungs <konstantin.hungs@beuth-hochschule.de>
2018-09-21 14:57:59 +0200

    remove unused app state

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { EditorState } from "draft-js";
 import * as React from "react";
 import { RTEEditor } from "./RTEEditor";
 
@@ -8,11 +7,6 @@ interface AppProps {
 export default class App extends React.Component<AppProps, {}> {
     constructor(props: AppProps) {
         super(props);
-        this.state = {editorState: EditorState.createEmpty()};
-    }
-
-    handleChange(e: EditorState) {
-        this.setState({ editorState: e });
     }
 
     render() {
